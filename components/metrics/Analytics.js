784a485bc49346b325cb0d5d92efcd59353de2cc
thnmtrn <thien.tran6@gmail.com>
2021-02-16 17:35:06 -0500

    edit dashboard links

--- components/metrics/Analytics.js
+++ components/metrics/Analytics.js
@@ -8,7 +8,7 @@ export default function Analytics() {
   const { data } = useSWR('/api/views', fetcher);
 
   const pageViews = format(data?.total);
-  const link = 'https://leerob.io';
+  const link = 'https://thientran.me';
 
   return <MetricCard header="All-Time Views" link={link} metric={pageViews} />;
 }
