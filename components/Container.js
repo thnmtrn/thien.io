31406a7e8aaa5e79a6918ed02defdcc729370f95
thnmtrn <thien.tran6@gmail.com>
2021-03-03 22:33:36 -0500

    new navbar again

--- components/Container.js
+++ components/Container.js
@@ -7,13 +7,10 @@ import Link from './Link'
 
 import Footer from '@/components/Footer';
 import MobileNav from './MobileNav';
+import Navbar from './Navbar';
 
 export default function Container(props) {
-  const [mounted, setMounted] = useState(false);
-  const { theme, setTheme } = useTheme();
 
-  // After mounting, we have access to the theme
-  useEffect(() => setMounted(true), []);
 
   const { children, ...customMeta } = props;
   const router = useRouter();
@@ -47,63 +44,8 @@ export default function Container(props) {
           <meta property="article:published_time" content={meta.date} />
         )}
       </Head>
-      <nav className="sticky-nav flex justify-between items-center max-w-4xl w-full p-8 my-0 md:my-8 mx-auto bg-white dark:bg-gray-900 bg-opacity-60">
-        <a href="#skip" className="sr-only focus:not-sr-only">
-          Skip to content
-        </a>
-        <button
-          aria-label="Toggle Dark Mode"
-          type="button"
-          className="bg-gray-200 dark:bg-gray-800 rounded p-3 h-10 w-10"
-          onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-        >
-          {mounted && (
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              stroke="currentColor"
-              className="h-4 w-4 text-gray-800 dark:text-gray-200"
-            >
-              {theme === 'dark' ? (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                />
-              ) : (
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                />
-              )}
-            </svg>
-          )}
-        </button>
-        <Link href="/">
-            <a className="text-gray-900 hover:text-cyan-400 transition text-4xl text-bold dark:text-gray-100 dark:hover:text-cyan-400">
-              t
-            </a>
-          </Link>
-        <div className="flex items-center text-base leading-5">
-          
-            <div className="hidden sm:block">
-              {headerNavLinks.map((link) => (
-                <Link
-                  key={link.title}
-                  href={link.href}
-                  className="p-1 font-medium text-gray-900 sm:p-4 dark:text-gray-100"
-                >
-                  {link.title}
-                </Link>
-              ))}
-            </div>
-            <MobileNav className=""/>
-          </div>
-      </nav>
+      
+      <Navbar/>
       <main
         id="skip"
         className="flex flex-col justify-center bg-white dark:bg-gray-900 px-8"
